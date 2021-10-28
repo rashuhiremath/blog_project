a692364ec053a0790035b1270a9cc2fb425b63b6
rashuhiremath <rashmi.hiremath94@gmail.com>
2021-10-28 16:57:40 +0200

    updated

--- src/libfile/fs_tool.js
+++ src/libfile/fs_tool.js
@@ -2,18 +2,20 @@ import fs from "fs-extra"
 import {fileURLToPath} from "url";
 import {join, dirname} from "path";
 
-const {readJSON,writeJSON,writeFile}= fs
+const {readJSON,writeJSON,writeFile,createReadStream}= fs
 
-const blogsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "../data/blogs.json")
+//const blogsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "../data/blogs.json")
+const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
   const imageFolderPath = join(process.cwd(),"./public/img/blogsImage")
-   const booksJSONPath = join(blogsJSONPath, "blogs.json")
+
+    const blogsJSONPath = join(dataFolderPath, "blogs.json")
 
 export const readBlogs = () => readJSON(blogsJSONPath)
 export const writeBlogs = content => writeJSON(blogsJSONPath, content) // content is array
 export const saveProductPicture = (filename,buffer) =>{
     writeFile(join(imageFolderPath,filename),buffer)}
 
-export const getBooksReadableStream = () => createReadStream(booksJSONPath)
+export const getBooksReadableStream = () => createReadStream(blogsJSONPath)
 
 
 
